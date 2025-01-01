eef15081129d67b76d6cf1fe1c4dc3d5e0b8901e
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-01-01 11:30:51 -0500

    Practice next project

---
+++ practice/app/page.tsx
@@ -0,0 +1,7 @@
+//import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-[300px] flex flex-col justify-center items-center">Hello, Tami!!!!</div>
+  );
+}
