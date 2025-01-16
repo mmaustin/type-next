67238d3e3b2419f161563be45e84f7d44b58c52f
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-01-16 13:35:56 -0500

    Installed zustand

--- practice/app/page.tsx
+++ practice/app/page.tsx
@@ -1,7 +1,21 @@
-//import Image from "next/image";
+'use client'
 
-export default function Home() {
-  return (
-    <div className="h-[300px] flex flex-col justify-center items-center">Hello, Tami!!!!</div>
-  );
-}
+import { Person } from '@/utils/types';
+
+function personHood(p: Person): string {
+  return p.name;
+};
+console.log(personHood({ name: 'Mccray', age: 49, occupation: true }));
+
+
+// interface Denard<T, U> {
+//   name: T
+//   age: U
+// }
+
+// const page = () => {
+//   return (
+//     <div></div>
+//   )
+// }
+export default personHood
