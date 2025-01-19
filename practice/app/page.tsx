0e503b095c87d9dcc98edd9adbf727d080970b45
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-01-19 13:24:31 -0500

    Returned homepage to original state

--- practice/app/page.tsx
+++ practice/app/page.tsx
@@ -1,21 +1,8 @@
-'use client'
-
-import { Person } from '@/utils/types';
-
-function personHood(p: Person): string {
-  return p.name;
-};
-console.log(personHood({ name: 'Mccray', age: 49, occupation: true }));
 
 
-// interface Denard<T, U> {
-//   name: T
-//   age: U
-// }
-
-// const page = () => {
-//   return (
-//     <div></div>
-//   )
-// }
-export default personHood
+function Page() {
+  return (
+    <div className="mt-8 display flex justify-center items-center">Page</div>
+  )
+}
+export default Page;
