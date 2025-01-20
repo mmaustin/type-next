56f64df3fadbcd604b7643f15af3a5f7e56f0811
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-01-20 15:38:50 -0500

    Experimented with zustand docs, useref usage

--- practice/providers/counter-store-provider.tsx
+++ practice/providers/counter-store-provider.tsx
@@ -1,8 +1,29 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 'use client'
 
-import { type ReactNode, createElement, useRef, useContext} from 'react';
+import { type ReactNode, createContext, useRef, useContext} from 'react';
 import { useStore } from 'zustand';
 import { type CounterStore, createCounterStore, initCounterStore} from '@/stores/counter-store';
 
 export type CounterStoreApi = ReturnType<typeof createCounterStore>;
+
+export const CounterStoreContext = createContext<CounterStoreApi | undefined>(undefined);
+
+export interface CounterStoreProviderProps {
+  children: ReactNode
+};
+
+export const CounterStoreProvider = ({children}: CounterStoreProviderProps) => {
+  const storeRef = useRef<CounterStoreApi>(undefined);
+
+  if(!storeRef.current){
+    storeRef.current = createCounterStore(initCounterStore())
+  }
+
+  return (
+    <CounterStoreContext.Provider value={storeRef.current}>
+      {children}
+    </CounterStoreContext.Provider>
+  );
+};
+
