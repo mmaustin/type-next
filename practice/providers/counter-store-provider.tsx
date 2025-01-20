42c5e4fbdaee0754926c9f7ca422b8f692b55f9a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-01-20 15:54:52 -0500

    Zustand useContext

--- practice/providers/counter-store-provider.tsx
+++ practice/providers/counter-store-provider.tsx
@@ -27,3 +27,14 @@ export const CounterStoreProvider = ({children}: CounterStoreProviderProps) => {
   );
 };
 
+export const useCounterStore = <T,>(
+  selector: (store: CounterStore) => T,  
+): T => {
+  const counterStoreContext = useContext(CounterStoreContext);
+
+  if(!counterStoreContext){
+    throw new Error(`useCounterStore must be used within CounterStoreProvider`);
+  };
+
+  return useStore(counterStoreContext, selector)
+}
