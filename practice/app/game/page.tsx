558a350e4060fcc837e6c99ede811abd522207a9
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-01-24 16:10:17 -0500

    Moved initial game components to a dedicated route

--- practice/app/game/page.tsx
+++ practice/app/game/page.tsx
@@ -1,8 +1,51 @@
+'use client'
+
+//import Square from "@/components/tic-tac-toe/Square";
 
 
-function page() {
+function TicTacToe() {
   return (
-    <div>page</div>
+    <div>
+      <p>Why Why Why?</p>
+      <p>because</p>
+    </div>
   )
 }
-export default page
+export default TicTacToe;
+
+// export type SquareProps = {
+//   value: number[],
+//   onSquareClick(id: number): string
+// }
+
+// function Page() {
+//   console.log(value);
+  
+//   const printThis = (i: number): number => {
+//     console.log('apple'[i]);
+//      return i;  
+//   }
+//   return (
+
+//     <Square value={[0,1,2]} onSquareClick={() => printThis(2)}/>
+//   )
+//     <>
+//     <button className="h-10 w-10 inline-flex justify-center items-center p-0 bg-red-300 border border-slate-400 outline-0 rounded-none font-bold text-base" onClick={() => console.log('apple')}>      
+//       987
+//     </button>
+//     </>
+//   )
+// }
+// export default Page;
+
+// export function SquareTest() {
+//   const printThis = (i: number): string => {
+//     console.log([i]);
+    
+//     return 'apple'[i];
+    
+//   }
+//   return (
+//     <Square value={[0,1,2]} onSquareClick={() => printThis(2)}/>
+//   )
+// };
--- practice/app/page.tsx
+++ practice/app/page.tsx
@@ -1,54 +1,16 @@
-// import HomePage from "@/components/pages/HomePage";
-'use client'
+import HomePage from "@/components/pages/HomePage";
 
-import Square from "@/components/tic-tac-toe/Square";
-
-// function Page() {
-//   return (
-//     <div className="mt-8 display flex justify-center items-center">
-//       <HomePage />
-//     </div>
-//   )
-// }
-// export default Page;
-
-
-
-
-
-// export type SquareProps = {
-//   value: number[],
-//   onSquareClick(id: number): string
-// }
 
 function Page() {
-  //console.log(value);
-  
-  const printThis = (i: number): number => {
-    console.log('apple'[i]);
-     return i;  
-  }
   return (
-
-    <Square value={[0,1,2]} onSquareClick={() => printThis(2)}/>
+    <div className="mt-8 display flex justify-center items-center">
+      <HomePage />
+    </div>
   )
-  //   <>
-  //   <button className="h-10 w-10 inline-flex justify-center items-center p-0 bg-red-300 border border-slate-400 outline-0 rounded-none font-bold text-base" onClick={() => console.log('apple')}>      
-  //     987
-  //   </button>
-  //   </>
-  // )
 }
 export default Page;
 
-// export function SquareTest() {
-//   const printThis = (i: number): string => {
-//     console.log([i]);
-    
-//     return 'apple'[i];
-    
-//   }
-//   return (
-//     <Square value={[0,1,2]} onSquareClick={() => printThis(2)}/>
-//   )
-// };
+
+
+
+
