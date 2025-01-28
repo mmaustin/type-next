7eca48659206038a1e478bffd78b9ea32f5a355b
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-01-27 21:11:59 -0500

    Added another value to the game store

--- practice/components/tic-tac-toe/Board.tsx
+++ practice/components/tic-tac-toe/Board.tsx
@@ -6,12 +6,20 @@ import { combine } from "zustand/middleware";
 
 
 const useGameStore = create(
-  combine({ squares: Array(9).fill(null) }, (set) => {
+  combine({ squares: Array(9).fill(null), xIsNext: true }, (set) => {
     return {
       setSquares: (nextSquares) => {
         set(state => ({
           squares:
             typeof nextSquares === 'function' ? nextSquares(state.squares) : nextSquares,
+        }))
+      },
+      setXIsNext: (nextXIsNext) => {
+        set(state => ({
+          xIsNext:
+            typeof nextXIsNext === 'function'
+              ? nextXIsNext(state.xIsNext)
+              : nextXIsNext,
         }))
       }
     }
