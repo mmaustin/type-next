8ee1fb3d25770296e8884d3a4aa662c836326412
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-02-01 16:44:57 -0500

    Altered calculate status type to take type string or null for winner paramenter

--- practice/components/tic-tac-toe/Board.tsx
+++ practice/components/tic-tac-toe/Board.tsx
@@ -39,8 +39,8 @@ function calculateWinner(squares: string[]): string | null {
     [2, 4, 6],
   ];
 
-  for (let i = 0; i < lines.length; i++){
-    const [a,b,c] = lines[i];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     };
@@ -48,17 +48,17 @@ function calculateWinner(squares: string[]): string | null {
   return null;
 };
 
-function calculateTurns(squares: string[]): number{
+function calculateTurns(squares: string[]): number {
   return squares.filter(square => !square).length;
 };
 
 type CalculateStatus = {
-  winner: string,
+  winner: string | null,
   turns: number,
   player: string
 };
 
-function calculateStatus({winner, turns, player}: CalculateStatus): string {
+function calculateStatus({ winner, turns, player }: CalculateStatus): string {
   if (!winner && !turns) return 'Draw';
   if (winner) return `Winner ${winner}`;
   return `Next player: ${player}`;
@@ -72,7 +72,8 @@ function Board() {
   const setSquares = useGameStore(state => state.setSquares);
   const winner = calculateWinner(squares);
   const turns = calculateTurns(squares);
-  const player = xIsNext ? 'X' : 'O'
+  const player = xIsNext ? 'X' : 'O';
+  const status = calculateStatus({ winner, turns, player })
 
   function handleClick(i: number) {
     if (squares[i] || winner) return;
@@ -83,12 +84,14 @@ function Board() {
   }
 
   return (
-
-    <div className="h-40 w-40" style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gridTemplateRows: 'repeat(3, 1fr', width: 'calc(3 * 2.5rem', height: 'calc(3 * 2.5rem', border: '1px solid #999' }}>
-      {squares.map((square, index) => (
-        <Square key={index} useIndex={index} value={square} onSquareClick={() => handleClick(index)} />
-      ))}
-    </div>
+    <>
+      <div style={{ marginBottom: '0.5' }} className="">{status}</div>
+      <div className="h-40 w-40" style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gridTemplateRows: 'repeat(3, 1fr', width: 'calc(3 * 2.5rem', height: 'calc(3 * 2.5rem', border: '1px solid #999' }}>
+        {squares.map((square, index) => (
+          <Square key={index} useIndex={index} value={square} onSquareClick={() => handleClick(index)} />
+        ))}
+      </div>
+    </>
   )
 }
 export default Board;
