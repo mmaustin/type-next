313c001affdb4e92f98177ff927005acbc712a05
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-02-08 11:05:00 -0500

    Removed default homepage, added new layout page for the home routes

--- practice/app/(root)/layout.tsx
+++ practice/app/(root)/layout.tsx
@@ -5,12 +5,12 @@ import { ReactNode } from "react"
 const RootLayout = ({children}: {children: ReactNode}) => {
   return (
     <>
-      <div className="">Navbar</div>
+      <div className=" bg-slate-600">Navbar</div>
         <main>
           {children}
         </main>
-      <div className="">Footerh</div>
+      <div className="">Footer</div>
     </>
   )
 }
-export default RootLayout
+export default RootLayout;
