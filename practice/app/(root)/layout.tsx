08bb581564ca40df21a16a5de7a475a4848ed8e5
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-02-08 11:24:35 -0500

    Added a new custom color to tailwind config file

--- practice/app/(root)/layout.tsx
+++ practice/app/(root)/layout.tsx
@@ -2,15 +2,11 @@ import { ReactNode } from "react"
 
 
 
-const RootLayout = ({children}: {children: ReactNode}) => {
+const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <>
-      <div className=" bg-slate-600">Navbar</div>
-        <main>
-          {children}
-        </main>
-      <div className="">Footer</div>
-    </>
+    <main>
+      {children}
+    </main>
   )
 }
 export default RootLayout;
