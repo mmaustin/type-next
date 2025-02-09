9447caa7b6a379ab3740c9668f16c4f25ec6ae24
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-02-09 11:13:41 -0500

    Created sidebar links array

---
+++ practice/constants/index.ts
@@ -0,0 +1,29 @@
+
+
+export const sidebarLinks = [
+  {
+    label: 'Home',
+    route: '/home',
+    imgUr: '/icons/home.svg'
+  },
+  {
+    label: 'Upcoming',
+    route: '/upcoming',
+    imgUr: '/icons/upcoming.svg'
+  },
+  {
+    label: 'Previous',
+    route: '/previous',
+    imgUr: '/icons/previous.svg'
+  },
+  {
+    label: 'Recordings',
+    route: '/recordings',
+    imgUr: '/icons/Video.svg'
+  },
+  {
+    label: 'Personal Room',
+    route: '/personal-room',
+    imgUr: '/icons/add-personal.svg'
+  },
+]
