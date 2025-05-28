de60f935a30b4bfdc9dae7898b70223e297d7141
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-05-28 14:28:57 -0400

    Added a placeholder image to navbar with next image

--- practice/components/Navbar.tsx
+++ practice/components/Navbar.tsx
@@ -1,11 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+
+import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-6 lg:px-10">
-      <Link href={'/'} className="text-white">
-        Home
+      <Link href={'/'} className="flex items-center gap-1">
+        <Image
+          src="/favicon.ico"
+          alt="Placeholder Image"
+          width={32}
+          height={32}
+          className="max-sm:size-10"
+        />
       </Link>
     </nav>
   )
