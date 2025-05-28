bba4cda8814a9821f136339e4dd3f194b85c0b02
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-05-28 15:09:01 -0400

    Added placeholder image to sheet trigger component

--- practice/components/MobileNav.tsx
+++ practice/components/MobileNav.tsx
@@ -1,9 +1,40 @@
-
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import Image from "next/image";
 
 
 const MobileNav = () => {
   return (
-    <div>MobileNav</div>
+    <section className="w-full max-w-[264px]">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Image
+            src="/favicon.ico"
+            alt="Placeholder Image"
+            width={32}
+            height={32}
+            className="cursor-pointer sm:hidden"
+          />
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your account
+              and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+
+    </section>
   )
 }
 export default MobileNav;
