a97c88384e0c4a6119147786635abc5b56a6f291
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-06-02 13:58:38 -0400

    Added initial homepage styling

--- practice/app/(root)/(home)/page.tsx
+++ practice/app/(root)/(home)/page.tsx
@@ -1,6 +1,15 @@
+
+
+
 const Home = () => {
   return (
-    <div className="text-white">Home</div>
+    <section className="flex size-full flex-col gap-10 text-white">
+      <div className="h-[300px] w-full rounded-[20px] bg-emerald-800">
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11 capitalize">
+          <h2>upcoming meeting at: 12:30 PM</h2>
+        </div>
+      </div>
+    </section>
   )
 }
-export default Home
+export default Home;
