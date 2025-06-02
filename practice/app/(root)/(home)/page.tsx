21160e72c6386875821940506d86801482bf831d
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-06-02 14:30:52 -0400

    Used date object to get and configure current date and time

--- practice/app/(root)/(home)/page.tsx
+++ practice/app/(root)/(home)/page.tsx
@@ -2,11 +2,19 @@
 
 
 const Home = () => {
+  const dateTime = new Date();
+  const dayOfWeek: string = dateTime.toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit'});
+  const time: string = ( new Intl.DateTimeFormat('en-US', {dateStyle: 'full'})).format();
+
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <div className="h-[300px] w-full rounded-[20px] bg-emerald-800">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11 capitalize">
-          <h2>upcoming meeting at: 12:30 PM</h2>
+          <h2 className=" max-w-[270px] rounded text-center">upcoming meeting at: 12:30 PM</h2>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-2xl font-bold">{dayOfWeek}</h1>
+            <h1 className="text-2xl font-bold">{time}</h1>
+          </div>
         </div>
       </div>
     </section>
