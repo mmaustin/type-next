133639e63fad4c1d3c94bcf9e57e368539b88039
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-06-02 15:30:26 -0400

    Installed react icons

--- practice/components/MeetingTypeList.tsx
+++ practice/components/MeetingTypeList.tsx
@@ -1,12 +1,16 @@
 'use client';
 
+import { BsPlusLg } from "react-icons/bs";
+
 
 
 const MeetingTypeList = () => {
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-col-4">
       <div className="bg-orange-400 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer" onClick={() => {}}>
-        Box 1
+        <div className="flex justify-center items-center size-12 rounded-[10px] bg-blue-600">
+          <BsPlusLg className=""/>
+        </div>
       </div>
     </section>
   )
