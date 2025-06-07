a41104e72c3b948bac4b74d4e61d59f10d1f4b2a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-06-07 14:10:58 -0400

    Added additional properties to modal props interface

--- practice/components/MeetingModal.tsx
+++ practice/components/MeetingModal.tsx
@@ -1,15 +1,20 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import React, {ReactNode} from "react";
+
 
 interface  MeetingModalProps {
   isOpen: boolean,
   onClose: () => void,
   title: string,
-  classname: string,
-  buttonText: string,
-  handleClick: () => void
+  classname?: string,
+  buttonText?: string,
+  handleClick?: () => void,
+  children?: ReactNode,
+  image?: string,
+  buttonIcon?: React.ReactNode
 }
 
-const MeetingModal = ({isOpen, onClose, title, classname, buttonText, handleClick}: MeetingModalProps) => {
+const MeetingModal = ({isOpen, onClose, title, classname, buttonText, handleClick, children, image, buttonIcon}: MeetingModalProps) => {
   return (
     <div>MeetingModal</div>
   )
