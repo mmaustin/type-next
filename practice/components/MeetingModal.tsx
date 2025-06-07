7f275bba8cdcf31f5c6a5b8f86b21f0648eb3d5c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-06-07 14:01:15 -0400

    Created interface in meeting modal compoent to handle props passed from the homepage

--- practice/components/MeetingModal.tsx
+++ practice/components/MeetingModal.tsx
@@ -1,9 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 
+interface  MeetingModalProps {
+  isOpen: boolean,
+  onClose: () => void,
+  title: string,
+  classname: string,
+  buttonText: string,
+  handleClick: () => void
+}
 
-
-const MeetingModal = () => {
+const MeetingModal = ({isOpen, onClose, title, classname, buttonText, handleClick}: MeetingModalProps) => {
   return (
     <div>MeetingModal</div>
   )
-}
+};
 export default MeetingModal;
