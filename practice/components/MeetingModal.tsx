b4171fb853e5ac5dadcaa0106f96e3ac265dc422
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-06-07 13:54:33 -0400

    Created meeting modal component to handle when state requires the modal to be open

---
+++ practice/components/MeetingModal.tsx
@@ -0,0 +1,9 @@
+
+
+
+const MeetingModal = () => {
+  return (
+    <div>MeetingModal</div>
+  )
+}
+export default MeetingModal;
--- practice/components/MeetingTypeList.tsx
+++ practice/components/MeetingTypeList.tsx
@@ -5,13 +5,18 @@ import { BsPlusLg } from "react-icons/bs";
 import FunctionCard from "./FunctionCard";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import MeetingModal from "./MeetingModal";
 
 const MeetingTypeList = () => {
 
   const router = useRouter();
   
   const [meetingState, setMeetingState] = useState<'isJoiningMeeting' | 'isScheduleMeeting' | 'isInstantMeeting' | undefined>();
-  console.log(meetingState)
+  
+  const createMeeting = (): void => {
+    console.log('createMeeting');
+    
+  };
 
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4">
@@ -19,6 +24,15 @@ const MeetingTypeList = () => {
       <FunctionCard cardText={'Schedule Meeting'} icon={<BsPlusLg />} handleClick={() => setMeetingState('isScheduleMeeting')} classname="bg-blue-600"/>
       <FunctionCard cardText={'View Recordings'} icon={<BsPlusLg />} handleClick={() => setMeetingState('isJoiningMeeting')} classname="bg-purple-600"/>
       <FunctionCard cardText={'meeting'} icon={<BsPlusLg />} handleClick={() => router.push('/recordings')} classname="bg-yellow-600"/>
+
+      <MeetingModal
+        isOpen={meetingState === 'isInstantMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title={'Start an Instant Meeting'}
+        classname='text-center'
+        buttonText='Start Meeting'
+        handleClick={createMeeting}
+      />
     </section>
   )
 }
