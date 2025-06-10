ee80cb1444d4e9ff20de149333d8bdbe02561bd0
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-06-09 20:18:42 -0400

    Added shadcn boilerplate dialog code, for altering

--- practice/components/MeetingModal.tsx
+++ practice/components/MeetingModal.tsx
@@ -1,8 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, {ReactNode} from "react";
+import React, { ReactNode } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 
-interface  MeetingModalProps {
+interface MeetingModalProps {
   isOpen: boolean,
   onClose: () => void,
   title: string,
@@ -14,9 +22,20 @@ interface  MeetingModalProps {
   buttonIcon?: React.ReactNode
 }
 
-const MeetingModal = ({isOpen, onClose, title, classname, buttonText, handleClick, children, image, buttonIcon}: MeetingModalProps) => {
+const MeetingModal = ({ isOpen, onClose, title, classname, buttonText, handleClick, children, image, buttonIcon }: MeetingModalProps) => {
   return (
-    <div>MeetingModal</div>
+    <Dialog>
+      <DialogTrigger>Open</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your account
+            and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   )
 };
 export default MeetingModal;
