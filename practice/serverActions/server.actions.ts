7080111eccf71a3bab2ba98524f3a3cf9b8960a4
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-06-11 16:08:25 -0400

    Used code to mimic a logged in user in order to generate a token

--- practice/serverActions/server.actions.ts
+++ practice/serverActions/server.actions.ts
@@ -1,23 +1,34 @@
 'use server';
 
-import { StreamClient } from '@stream-io/node-sdk'
+import { StreamClient, UserRequest } from '@stream-io/node-sdk'
 
 const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 const apiSecret = process.env.STREAM_SECRET_KEY;
 
-export const tokenProvider = async() => {
 
+
+export const tokenProvider = async() => {
+  
   if(!apiKey) throw new Error('No API key');
   if(!apiSecret) throw new Error('No secret key');
 
   const client = new StreamClient(apiKey, apiSecret);
+
+  const userId = '1';
+  const newUser: UserRequest = {
+    id: userId,
+    role: 'user',
+    name: 'McCray'
+  };
+
+  await client.upsertUsers([newUser]);
   
   const exp = Math.round(new Date().getTime() / 1000) + 60 * 60;
   const issued = Math.floor(Date.now() / 1000) - 60;
 
   const vailidity = 60 * 60;
   
-  const token = client.generateUserToken({user_id: '1', validity_in_seconds: vailidity, exp, issued });
+  const token = client.generateUserToken({user_id: userId, validity_in_seconds: vailidity, exp, issued });
 
   return token;
 
