1d19a482bd20766c24f898cf01652fda5d883b68
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-06-14 14:18:12 -0400

    Test commit, new vscode install

--- practice/serverActions/server.actions.ts
+++ practice/serverActions/server.actions.ts
@@ -5,10 +5,10 @@ import { StreamClient, UserRequest } from '@stream-io/node-sdk'
 const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 const apiSecret = process.env.STREAM_SECRET_KEY;
 
-export const tokenProvider = async() => {
-  
-  if(!apiKey) throw new Error('No API key');
-  if(!apiSecret) throw new Error('No secret key');
+export const tokenProvider = async () => {
+
+  if (!apiKey) throw new Error('No API key');
+  if (!apiSecret) throw new Error('No secret key');
 
   const client = new StreamClient(apiKey, apiSecret);
 
@@ -18,15 +18,16 @@ export const tokenProvider = async() => {
     role: 'user',
     name: 'McCray'
   };
+  console.log('test');
 
   await client.upsertUsers([newUser]);
-  
+
   const exp = Math.round(new Date().getTime() / 1000) + 60 * 60;
   const issued = Math.floor(Date.now() / 1000) - 60;
 
   const vailidity = 60 * 60;
-  
-  const token = client.generateUserToken({user_id: userId, validity_in_seconds: vailidity, exp, issued });
+
+  const token = client.generateUserToken({ user_id: userId, validity_in_seconds: vailidity, exp, issued });
 
   return token;
 
