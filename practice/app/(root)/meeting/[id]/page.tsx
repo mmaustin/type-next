a530b52a02910fe313ef9a9de05c900f94bcfb08
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-06-15 11:01:21 -0400

    Added an id property to params Promise to allow for proper object destructuring

--- practice/app/(root)/meeting/[id]/page.tsx
+++ practice/app/(root)/meeting/[id]/page.tsx
@@ -1,16 +1,14 @@
 'use client';
 
 import { use } from "react";
-import { User } from "@stream-io/video-react-sdk";
+//import { User } from "@stream-io/video-react-sdk";
 
-const Meeting = ({params}: {params: Promise<string>}) => {
+const Meeting = ({params}: {params: Promise<{id: string}>}) => {
 
-  const user: User = {id: '1', name: 'mccray'};
-  console.log(user);
+  //const user: User = {id: '1', name: 'mccray'};
   
-   
   const paramId = use(params)
-  const id = Object.values(paramId)[0];
+  const {id} = paramId;
   
   return (
     <div className="text-zinc-50">Meeting Room #: {id} </div>
