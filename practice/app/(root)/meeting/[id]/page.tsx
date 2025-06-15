6bba89fc4ad04fd0e04edcec404311dede4e20bc
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-06-15 10:53:12 -0400

    With React.use, extracted the params promise id

--- practice/app/(root)/meeting/[id]/page.tsx
+++ practice/app/(root)/meeting/[id]/page.tsx
@@ -1,9 +1,16 @@
+'use client';
 
+import { use } from "react";
+import { User } from "@stream-io/video-react-sdk";
 
+const Meeting = ({params}: {params: Promise<string>}) => {
 
-const Meeting = async ({params}: {params: {id: string}}) => {
+  const user: User = {id: '1', name: 'mccray'};
+  console.log(user);
   
-  const {id} = await params;
+   
+  const paramId = use(params)
+  const id = Object.values(paramId)[0];
   
   return (
     <div className="text-zinc-50">Meeting Room #: {id} </div>
