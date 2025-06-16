fc9ee5cace4af322d485950cf0d057c103032ff3
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-06-16 12:07:01 -0400

    Completed use get call by id hook

--- practice/hooks/useGetCallById.ts
+++ practice/hooks/useGetCallById.ts
@@ -1,10 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client';
 
 import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
 import { useEffect, useState } from "react";
 
-export const useGetCallById = (id: string | string[]): Call | void => {
+export const useGetCallById = (id: string | string[]) => {
   const [call, setCall] = useState<Call>();
   const [isCallLoading, setIsCallLoading] = useState(true);
 
@@ -13,8 +12,18 @@ export const useGetCallById = (id: string | string[]): Call | void => {
   useEffect(() => {
     if(!client) return;
     
-    
+    const loadCall = async() => {
+      const {calls} = await client.queryCalls({
+        filter_conditions : { id }
+      });
+      if(calls.length > 0) setCall(calls[0]);
+      setIsCallLoading(false);
+    };
+
+    loadCall();
 
   }, [client, id]);
 
-}
+  return {call, isCallLoading};
+
+};
