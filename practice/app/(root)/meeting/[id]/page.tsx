a88bd0d621ac2f82d95509b2500da210b0d4a626
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-06-16 12:26:52 -0400

    Called custom call hook in meeting id route

--- practice/app/(root)/meeting/[id]/page.tsx
+++ practice/app/(root)/meeting/[id]/page.tsx
@@ -3,20 +3,25 @@
 
 import MeetingRoom from "@/components/MeetingRoom";
 import MeetingSetup from "@/components/MeetingSetup";
+import { useGetCallById } from "@/hooks/useGetCallById";
 import { StreamCall, User, StreamTheme } from "@stream-io/video-react-sdk";
 import { use, useState } from "react";
 
 const Meeting = ({params}: {params: Promise<{id: string}>}) => {
+  const paramId = use(params)
+  const {id} = paramId;
+  
 
   const [isSetupComplete, setIsSetupComplete] = useState<boolean>(false);
 
   const user: User = {id: '1', name: 'mccray'};
   
-  const paramId = use(params)
-  const {id} = paramId;
+  const {call, isCallLoading} = useGetCallById(id);
+  
+  if(!user || !isCallLoading) return <p>Something Rotten Is Going On!</p>
   
   return (
-    <StreamCall>
+    <StreamCall call={call}>
       <StreamTheme>
         {!isSetupComplete ? (
           <MeetingSetup />
