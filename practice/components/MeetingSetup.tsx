ac5b4679bcc01198a534154a8c75110b90788f3a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-06-17 17:10:11 -0400

    Enabled camera and microphone with stream react useCall

--- practice/components/MeetingSetup.tsx
+++ practice/components/MeetingSetup.tsx
@@ -1,8 +1,32 @@
+'use client';
 
+import { VideoPreview, useCall } from "@stream-io/video-react-sdk";
+import { useEffect, useState } from "react";
 
 const MeetingSetup = () => {
+
+  const [isMicCamToggleOn, setIsMicCamToggleOn] = useState(false);
+
+  const call = useCall();
+
+  useEffect(() => {
+    if(isMicCamToggleOn){
+      call?.camera.disable();
+      call?.microphone.disable();
+    } else {
+      call?.camera.enable();
+      call?.microphone.enable();
+    };
+
+  }, [isMicCamToggleOn, call?.camera, call?.microphone]);
+
   return (
-    <div className="text-white">MeetingSetup</div>
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
+      <h1 className="text-2xl font-bold">
+        Setup
+      </h1>
+      <VideoPreview />
+    </div>
   )
 }
 export default MeetingSetup;
