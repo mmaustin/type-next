30a2884d6c528716be376102ec3753ac915db5d2
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-06-21 12:15:02 -0400

    Created call layout component, a switch statement that will determine meeting room layout

--- practice/components/MeetingRoom.tsx
+++ practice/components/MeetingRoom.tsx
@@ -1,19 +1,31 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 'use client';
 
+import { PaginatedGridLayout, SpeakerLayout } from "@stream-io/video-react-sdk";
 import { useState } from "react";
 
 type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right';
 
 const MeetingRoom = () => {
 
-  const [layout, setLayout] = useState<CallLayoutType>('speaker-left')
+  const [layout, setLayout] = useState<CallLayoutType>('speaker-left');
+
+  const CallLayout = () => {
+    switch (layout) {
+      case 'grid':
+        return <PaginatedGridLayout />
+      case 'speaker-right':
+        return <SpeakerLayout participantsBarPosition={"left"} />
+      default:
+        return <SpeakerLayout participantsBarPosition={"right"} />
+    }
+  }
 
   return (
     <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
       <div className="relative flex size-full items-center justify-center">
         <div className="flex size-full max-w-[1000px] items-center">
-          {layout}
+          <CallLayout />
         </div>
       </div>
     </section>
