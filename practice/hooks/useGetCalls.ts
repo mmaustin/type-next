62726f02f226b7f38dbc43c5014c5d351d07308a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-06-29 11:57:57 -0400

    Implemented try/catch/finally portion of get calls hook

--- practice/hooks/useGetCalls.ts
+++ practice/hooks/useGetCalls.ts
@@ -2,12 +2,40 @@
 "use client";
 
 import { Call, useStreamVideoClient, User } from "@stream-io/video-react-sdk";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const useGetCall = () => {
   const [calls, setCalls] = useState<Call[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const client = useStreamVideoClient();
-  const user: User = {id: '1', name: 'mccray'};
+  const user: User = { id: '1', name: 'mccray' };
+
+  useEffect(() => {
+    const loadCalls = async () => {
+      if (!client || !user?.id) return;
+      setIsLoading(true);
+
+      try {
+
+        const { calls } = await client.queryCalls({
+          sort: [{field: 'starts_at', direction: -1}],
+          filter_conditions: {
+            starts_at: { $exists: true},
+            $or: [
+              {created_by_user_id: user.id},
+              {members: {$in: [user.id]}},
+            ]
+          }
+        });
+        setCalls(calls);
+      } catch (error) {
+        console.log(error);
+
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    loadCalls();
+  }, [client, user?.id])
 };
