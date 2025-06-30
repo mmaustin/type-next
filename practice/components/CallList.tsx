9af630c459e194ec9f5931e74870309e9403bf00
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-06-30 15:08:14 -0400

    Created function with switch statement to access the proper set of calls from the Calls object

--- practice/components/CallList.tsx
+++ practice/components/CallList.tsx
@@ -2,12 +2,34 @@
 'use client';
 
 import { useGetCall } from "@/hooks/useGetCalls";
+import { Call } from "@stream-io/video-react-sdk";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 type CallListProps = {
   type: "upcoming" | "recordings" | "ended"
 }
 
 const CallList = ({type } : CallListProps) => {
+
+  const { endedCalls, upcomingCalls, callRecordings, isLoading} = useGetCall();
+  const router = useRouter();
+
+  const [recordings, getRecordings] = useState<Call[]>([])
+
+  const getCalls = () => {
+    switch(type) {
+      case 'ended':
+        return endedCalls;
+      case 'recordings':
+        return recordings;
+      case 'upcoming':
+        return upcomingCalls;
+      default:
+        return [];
+    }
+  }
+  
   return (
     <div>CallList</div>
   )
