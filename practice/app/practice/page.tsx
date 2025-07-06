90ba7d2b67c18750e3731e93e10daf41af88cca4
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-07-06 17:20:53 -0400

    Installed leaflet

--- practice/app/practice/page.tsx
+++ practice/app/practice/page.tsx
@@ -1,24 +1,33 @@
 'use client'
 
-// import { MapContainer, TileLayer, Marker } from 'react-leaflet';
-import {FaAccusoft} from "react-icons/fa"
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+//import {FaAccusoft} from "react-icons/fa"
+import {icon} from 'leaflet';
+const iconUrl =
+  'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon-2x.png';
+const markerIcon = icon({
+  iconUrl: iconUrl,
+  iconSize: [20, 30],
+});
+
 
 const page = () => {
 
-  // const position = [40.843722, -73.917502] as [number, number]
+  const position = [40.843, -73.91] as [number, number]
 
   return (
-  //   <MapContainer zoom={13} scrollWheelZoom={false}>
-  //   <TileLayer
-  //     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  //     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  //   />
-  //   <Marker position={position}>
-  //   </Marker>
-  // </MapContainer>
-  <div className="">
-    <FaAccusoft className="text-white"/>
-  </div>
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position} icon={markerIcon}>
+        ppp
+      </Marker>
+    </MapContainer>
+    // <div className="">
+    //   <FaAccusoft className="text-white"/>
+    // </div>
   )
 }
 export default page
