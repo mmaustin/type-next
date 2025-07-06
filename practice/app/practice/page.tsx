4165d02e609d0be396fdfcfa899e7dcbd260b246
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-07-06 16:57:02 -0400

    Installed types/leaflets for react-leaflets

--- practice/app/practice/page.tsx
+++ practice/app/practice/page.tsx
@@ -1,24 +1,39 @@
 'use client'
 
+// import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import {FaAccusoft} from "react-icons/fa"
 
 const page = () => {
+
+  // const position = [40.843722, -73.917502] as [number, number]
+
   return (
-    <div>page</div>
+  //   <MapContainer zoom={13} scrollWheelZoom={false}>
+  //   <TileLayer
+  //     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  //     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  //   />
+  //   <Marker position={position}>
+  //   </Marker>
+  // </MapContainer>
+  <div className="">
+    <FaAccusoft className="text-white"/>
+  </div>
   )
 }
 export default page
 
-type Container = {
-  itemOne: string,
-  itemTwo: number,
-  itemThree: boolean
-};
+// type Container = {
+//   itemOne: string,
+//   itemTwo: number,
+//   itemThree: boolean
+// };
 
-const firstContainer: Container = {
-  itemOne: 'some text',
-  itemTwo: 99,
-  itemThree: true
-};
+// const firstContainer: Container = {
+//   itemOne: 'some text',
+//   itemTwo: 99,
+//   itemThree: true
+// };
 
-console.log(firstContainer.itemTwo);
+// console.log(firstContainer.itemTwo);
 
