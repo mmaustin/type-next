ea6aecdbe0879c15246d17fc7a38779180a275e3
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-07-10 15:31:38 -0400

    Created client component to unwrap promise prop with react use

---
+++ practice/components/PromisePropComponent.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { use } from "react";
+
+
+const PromisePropComponent = ({proppy}: {proppy: Promise<string>}) => {
+
+
+  return (
+    <div>PromisePropComponent</div>
+  )
+}
+export default PromisePropComponent;
